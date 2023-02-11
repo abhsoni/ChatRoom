525581313badc81382f5aa02c670ed11da4d5489
asoni11999 <asoni@ar.iitr.ac.in>
2023-02-11 17:43:21 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat Room</h3>
+          <input
+            type="text"
+            placeholder="Enter your Secret Code..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter the Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+      <img
+        src={"CLASSROOM.png"}
+        style={{
+          height: "100vh",
+          width: "100%",
+          position: "fixed",
+          background: "black",
+          zIndex: "-1",
+        }}
+        alt="logo"
+      />
+    </div>
+  );
+}
+
+export default App;
